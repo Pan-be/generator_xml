6b15b9d39a294f19af1c5bac85eee35815da4896
Pan.Be <konradbieniek@gmail.com>
2024-11-19 13:29:08 +0100

    change lang

--- generate_xml.py
+++ generate_xml.py
@@ -2,11 +2,12 @@
 import pandas as pd
 import xml.etree.ElementTree as ET
 
-# Wczytaj plik Excel
-file_path = '/home/kbieniek/Desktop/Lucrum/Opisy gier.xlsx'  # Zastąp 'path_to_your_file' właściwą ścieżką do pliku
+# load excel file
+file_path = '/path_to_file/Opisy gier.xlsx'  # Replace path_to_file with the correct file path
+
 df_lg = pd.read_excel(file_path, sheet_name='LG')
 
-# Funkcja do utworzenia struktury XML dla jednego rekordu
+# Function to create an XML structure for one record
 def create_xml_record(row):
     record = ET.Element('record')
     
@@ -32,15 +33,19 @@ def create_xml_record(row):
     
     return record
 
-# Utwórz główny element XML
+# Create a root XML element
+
 root = ET.Element('records')
 
-# Dodaj każdy rekord do głównego elementu
+# Add each record to the main item
+
 for _, row in df_lg.iterrows():
     root.append(create_xml_record(row))
 
-# Utwórz drzewo XML i zapisz do pliku
-xml_file_path = '/home/kbieniek/Desktop/Lucrum/gry.xml'  # Zastąp 'path_to_save' właściwą ścieżką do zapisu pliku
+# Create an XML tree and save to file
+
+xml_file_path = '/path_to_file/gry.xml'  # Zreplace path_to_save with the correct path to save the file
+
 tree = ET.ElementTree(root)
 tree.write(xml_file_path, encoding='utf-8', xml_declaration=True)
 
